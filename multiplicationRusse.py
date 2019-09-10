63ac411973545273017d3f90adb1e4fccb3d9916
mathieu perron <35858630+mathieu2em@users.noreply.github.com>
2019-09-09 22:20:30 -0400

    simplifying code

--- multiplicationRusse.py
+++ multiplicationRusse.py
@@ -5,8 +5,6 @@ inity = sys.argv[2]
 
 x = int(initx, 10)
 y = int(inity, 10)
-
-nonParElems = []
 sum = 0
 
 def multRuss():
@@ -14,16 +12,13 @@ def multRuss():
     global y
     global sum
     if x <= 1:
-        for var in nonParElems:
-            sum+=var
         print("the result of " + initx + " times "
               + inity + " is " + str(sum))
         return sum
     else:
         x = x>>1
         y = y<<1
-        if (y%2==0) ^ (x%2==0)  : nonParElems.append(y);
-        print(nonParElems)
+        if (y%2==0) ^ (x%2==0)  : sum += y;
         return multRuss();
 
 multRuss()
